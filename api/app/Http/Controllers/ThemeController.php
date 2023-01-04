7a52b2a2958b992de7ec24cbd45894c9911a46b1
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2023-01-04 13:54:27 +0100

    fix(theme): fix controller

--- api/app/Http/Controllers/ThemeController.php
+++ api/app/Http/Controllers/ThemeController.php
@@ -6,6 +6,7 @@ use App\Models\Theme;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Http\Resources\ThemeResource;
+use App\Models\Question;
 use Illuminate\Support\Facades\Http;
 
 class ThemeController extends Controller
@@ -20,18 +21,14 @@ class ThemeController extends Controller
         $allThemes = Theme::all();
 
         $themes = [];
-        foreach($allThemes as $theme){
-            //$id_theme = $theme->id;
-            if(Question::where('theme_id', $id_theme)){
+        foreach ($allThemes as $theme)
+            if (Question::where('theme_id', $theme->id)->count() > 0)
                 $themes[] = $theme;
-            }
-        }
 
-        foreach ($themes as $theme) {
+        foreach ($themes as $theme)
             $theme->image = Http::get('https://api.unsplash.com/search/photos/?client_id=' . env('CLIENT_ID') . '&query=' . $theme->name)['results'][0]['urls']['thumb'];
-        }
 
-        return $this->success($themes);
+        return $this->success(ThemeResource::collection($themes));
     }
 
     /**
--- api/app/Http/Resources/ThemeResource.php
+++ api/app/Http/Resources/ThemeResource.php
@@ -17,6 +17,7 @@ class ThemeResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'image' => $this->image,
         ];
     }
 }

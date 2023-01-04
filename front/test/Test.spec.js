55cbd3772e7bc59087ad8eca0da33e9e56f9acdc
sakerlucas <sakerlucas@gmail.com>
2023-01-04 08:25:57 +0100

    test front

--- front/test/Test.spec.js
+++ front/test/Test.spec.js
@@ -14,9 +14,19 @@ describe('Test', () => {
     expect(wrapper.vm.score).toBeUndefined()
   })
 
+  test('score is not defined', () => {
+    const wrapper = mount(Index)
+    expect(wrapper.vm.score).not.toBeDefined()
+  })
+
   test('name is empty', () => {
     const wrapper = mount(Index)
     expect(wrapper.vm.name).toBe('')
+  })
+
+  test('name length is not > 0', () => {
+    const wrapper = mount(Index)
+    expect(wrapper.vm.name.length).not.toBeGreaterThan(0)
   })
 
   test('number of questions is 1', () => {
@@ -46,16 +56,21 @@ describe('Test', () => {
     expect(wrapper.vm.startGame).toHaveBeenCalled()
   })
 
+  test('Game is a Vue instance', () => {
+    const wrapper = mount(Game)
+    expect(wrapper.vm).toBeTruthy()
+  })
+
   // test('Game is a Vue instance', () => {
   //   const wrapper = mount(Index)
-  //   const startGame = jest.fn()
+  //   wrapper.vm.startGame = jest.fn()
   //   wrapper.setData({ name: "test" })
   //   wrapper.setData({ difficulty: 1 })
   //   wrapper.setData({ number: 1 })
   //   wrapper.setData({ started: true })
-  //   wrapper.vm.startGame()
-  //   //cherche si game est lancé
-  //   expect(wrapper.findComponent(Game).exists()).toBe(true)
+  //   wrapper.find('button').trigger('click')
+  //   const game = wrapper.findComponent(Game)
+  //   expect(game.exists()).toBe(true)
   // })
 
 })
